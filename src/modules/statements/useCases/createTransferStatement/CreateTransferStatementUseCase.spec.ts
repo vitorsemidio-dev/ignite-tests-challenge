e8b2c3d5cf62c384fe392b29dacbf04f4f4af801
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-02-11 17:13:59 -0300

    :recycle: use factories function for CreateTransferStatementUseCase test

--- src/modules/statements/useCases/createTransferStatement/CreateTransferStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createTransferStatement/CreateTransferStatementUseCase.spec.ts
@@ -1,48 +1,24 @@
 import { v4 as uuidv4 } from "uuid";
+import { makeCreateTransferStatementUseCase } from "../../../../__tests__/CreateTransferStatementUseCaseFactory";
 import {
   makeDepositStatementDto,
   makeTransferStatementDto,
 } from "../../../../__tests__/StatementFactory";
-import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
-import { ICreateUserDTO } from "../../../users/useCases/createUser/ICreateUserDTO";
+import { makeUserDto } from "../../../../__tests__/UserFactory";
 import { OperationType } from "../../entities/Statement";
-import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
 import { CreateTransferStatementError } from "./CreateTransferStatementError";
-import { CreateTransferStatementUseCase } from "./CreateTransferStatementUseCase";
-
-const makeDTO = () => {
-  const userDefault = {
-    email: "test@email.com",
-    name: "test",
-    password: "test",
-  };
-  const makeUserDto = ({
-    email,
-    name,
-    password,
-  }: ICreateUserDTO = userDefault) => ({
-    email: email || userDefault.email,
-    name: name || userDefault.name,
-    password: password || userDefault.password,
-  });
-
-  return {
-    makeUserDto,
-  };
-};
 
 const makeSut = () => {
-  const usersRepository = new InMemoryUsersRepository();
-  const statementsRepository = new InMemoryStatementsRepository();
-  const createTransferStatmentUseCase = new CreateTransferStatementUseCase(
+  const {
     usersRepository,
-    statementsRepository
-  );
+    statementsRepository,
+    createTransferStatementUseCase,
+  } = makeCreateTransferStatementUseCase();
 
   return {
     usersRepository,
     statementsRepository,
-    createTransferStatmentUseCase,
+    createTransferStatementUseCase,
   };
 };
 
@@ -51,22 +27,19 @@ describe("CreateTransferStatementUseCase", () => {
     const {
       usersRepository,
       statementsRepository,
-      createTransferStatmentUseCase,
+      createTransferStatementUseCase,
     } = makeSut();
-    const { makeUserDto } = makeDTO();
 
     const userSender = await usersRepository.create(
       makeUserDto({
         email: "sender@email.com",
         name: "sender",
-        password: "12345678",
       })
     );
     const userReceiver = await usersRepository.create(
       makeUserDto({
         email: "receiver@email.com",
         name: "receiver",
-        password: "12345678",
       })
     );
     await statementsRepository.create(
@@ -82,25 +55,22 @@ describe("CreateTransferStatementUseCase", () => {
       receiver_id: userReceiver.id!,
     });
 
-    await createTransferStatmentUseCase.execute(transferStatementDto);
+    await createTransferStatementUseCase.execute(transferStatementDto);
   });
 
   it(`should not be able to create new statements with type '${OperationType.TRANSFER}' with insufficient funds`, async () => {
-    const { usersRepository, createTransferStatmentUseCase } = makeSut();
-    const { makeUserDto } = makeDTO();
+    const { usersRepository, createTransferStatementUseCase } = makeSut();
 
     const userSender = await usersRepository.create(
       makeUserDto({
         email: "sender@email.com",
         name: "sender",
-        password: "12345678",
       })
     );
     const userReceiver = await usersRepository.create(
       makeUserDto({
         email: "receiver@email.com",
         name: "receiver",
-        password: "12345678",
       })
     );
 
@@ -110,18 +80,16 @@ describe("CreateTransferStatementUseCase", () => {
     });
 
     await expect(async () => {
-      await createTransferStatmentUseCase.execute(transferStatementDto);
+      await createTransferStatementUseCase.execute(transferStatementDto);
     }).rejects.toBeInstanceOf(CreateTransferStatementError.InsufficientFunds);
   });
   it(`should not be able to create new statements with type '${OperationType.TRANSFER}' to non-exists user`, async () => {
-    const { usersRepository, createTransferStatmentUseCase } = makeSut();
-    const { makeUserDto } = makeDTO();
+    const { usersRepository, createTransferStatementUseCase } = makeSut();
 
     const userSender = await usersRepository.create(
       makeUserDto({
         email: "sender@email.com",
         name: "sender",
-        password: "12345678",
       })
     );
 
@@ -131,19 +99,17 @@ describe("CreateTransferStatementUseCase", () => {
     });
 
     await expect(async () => {
-      await createTransferStatmentUseCase.execute(transferStatementDto);
+      await createTransferStatementUseCase.execute(transferStatementDto);
     }).rejects.toBeInstanceOf(CreateTransferStatementError.UserNotFound);
   });
 
   it(`should not be able to create new statements with type '${OperationType.TRANSFER}' to yourself`, async () => {
-    const { usersRepository, createTransferStatmentUseCase } = makeSut();
-    const { makeUserDto } = makeDTO();
+    const { usersRepository, createTransferStatementUseCase } = makeSut();
 
     const userSender = await usersRepository.create(
       makeUserDto({
         email: "sender@email.com",
         name: "sender",
-        password: "12345678",
       })
     );
 
@@ -153,7 +119,7 @@ describe("CreateTransferStatementUseCase", () => {
     });
 
     await expect(async () => {
-      await createTransferStatmentUseCase.execute(transferStatementDto);
+      await createTransferStatementUseCase.execute(transferStatementDto);
     }).rejects.toBeInstanceOf(CreateTransferStatementError.OperationForbidden);
   });
 });
