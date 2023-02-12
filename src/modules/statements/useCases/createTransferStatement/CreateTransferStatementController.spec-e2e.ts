11fc776ae262931b9ecc11990aa73986d7262d67
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-02-12 03:30:13 -0300

    :white_check_mark: add CreateTransferStatementController e2e test

--- src/modules/statements/useCases/createTransferStatement/CreateTransferStatementController.spec-e2e.ts
+++ src/modules/statements/useCases/createTransferStatement/CreateTransferStatementController.spec-e2e.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../../../app";
 import { makeDepositStatementDto } from "../../../../__tests__/StatementFactory";
+import { makeJWTToken } from "../../../../__tests__/TokenFactory";
 import { makeUser, makeUserDto } from "../../../../__tests__/UserFactory";
 import { ICreateUserDTO } from "../../../users/useCases/createUser/ICreateUserDTO";
 import { TestDatabase } from "./../../../../__tests__/TestDbConnection";
@@ -43,10 +44,10 @@ describe("Create Transfer Statement Controller", () => {
 
   it("should be able to create a new transfer statement", async () => {
     const sender = await makeE2EUser({
-      email: "sender1@email.com",
+      email: "sender@email.com",
     });
     const receiver = await makeE2EUser({
-      email: "receiver1@email.com",
+      email: "receiver@email.com",
     });
     const depositStatementDto = makeDepositStatementDto();
     await request(app)
@@ -69,10 +70,10 @@ describe("Create Transfer Statement Controller", () => {
   it("should not be able to create a new transfer statement if the sender does not have enough funds", async () => {
     const expectedError = new CreateTransferStatementError.InsufficientFunds();
     const sender = await makeE2EUser({
-      email: "sender1@email.com",
+      email: "sender@email.com",
     });
     const receiver = await makeE2EUser({
-      email: "receiver1@email.com",
+      email: "receiver@email.com",
     });
 
     const requestBody = {
@@ -93,9 +94,88 @@ describe("Create Transfer Statement Controller", () => {
     );
   });
 
-  it("should not be able to create a new transfer statement if the sender does not exist", async () => {});
+  it("should not be able to create a new transfer statement if the sender does not exist", async () => {
+    const expectedError = new CreateTransferStatementError.UserNotFound(
+      "Sender not found"
+    );
+    const sender = makeUser({
+      email: "sender@email.com",
+    });
+    const { token: senderToken } = makeJWTToken(sender);
+    const receiver = await makeE2EUser({
+      email: "receiver@email.com",
+    });
 
-  it("should not be able to create a new transfer statement if the receiver does not exist", async () => {});
+    const requestBody = {
+      amount: 100,
+      description: "transfer_description",
+    };
 
-  it("should not be able to create a new transfer statement if the sender is the same as the receiver", async () => {});
+    const { body: transferStatementBody } = await request(app)
+      .post(`/api/v1/statements/transfers/${receiver.user.id}`)
+      .set("Authorization", `Bearer ${senderToken}`)
+      .send(requestBody)
+      .expect(expectedError.statusCode);
+
+    expect(transferStatementBody).toEqual(
+      expect.objectContaining({
+        message: expectedError.message,
+      })
+    );
+  });
+
+  it("should not be able to create a new transfer statement if the receiver does not exist", async () => {
+    const expectedError = new CreateTransferStatementError.UserNotFound(
+      "Receiver not found"
+    );
+    const sender = await makeE2EUser({
+      email: "sender@email.com",
+    });
+    const receiver = makeUser({
+      email: "receiver@email.com",
+    });
+
+    const requestBody = {
+      amount: 100,
+      description: "transfer_description",
+    };
+
+    const { body: transferStatementBody } = await request(app)
+      .post(`/api/v1/statements/transfers/${receiver.id}`)
+      .set("Authorization", `Bearer ${sender.token}`)
+      .send(requestBody)
+      .expect(expectedError.statusCode);
+
+    expect(transferStatementBody).toEqual(
+      expect.objectContaining({
+        message: expectedError.message,
+      })
+    );
+  });
+
+  it("should not be able to create a new transfer statement if the sender is the same as the receiver", async () => {
+    const expectedError = new CreateTransferStatementError.OperationForbidden(
+      "You can't transfer to yourself"
+    );
+    const sender = await makeE2EUser({
+      email: "sender@email.com",
+    });
+
+    const requestBody = {
+      amount: 100,
+      description: "transfer_description",
+    };
+
+    const { body: transferStatementBody } = await request(app)
+      .post(`/api/v1/statements/transfers/${sender.user.id}`)
+      .set("Authorization", `Bearer ${sender.token}`)
+      .send(requestBody)
+      .expect(expectedError.statusCode);
+
+    expect(transferStatementBody).toEqual(
+      expect.objectContaining({
+        message: expectedError.message,
+      })
+    );
+  });
 });
