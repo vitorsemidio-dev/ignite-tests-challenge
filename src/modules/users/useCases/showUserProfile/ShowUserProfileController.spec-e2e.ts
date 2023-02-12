e27841f527be291a2789fcb49629701aa7efd7c3
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-02-12 01:02:21 -0300

    :white_check_mark: add ShowUserProfileController e2e test

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec-e2e.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.spec-e2e.ts
@@ -1,6 +1,9 @@
 import request from "supertest";
 import { app } from "../../../../app";
 import { TestDatabase } from "../../../../__tests__/TestDbConnection";
+import { makeJWTToken } from "../../../../__tests__/TokenFactory";
+import { makeUser, makeUserDto } from "../../../../__tests__/UserFactory";
+import { ShowUserProfileError } from "./ShowUserProfileError";
 
 describe("ShowUserProfileController", () => {
   beforeEach(async () => {
@@ -9,13 +12,50 @@ describe("ShowUserProfileController", () => {
     await TestDatabase.migrate();
   });
 
-  afterEach(async () => {
+  afterAll(async () => {
     await TestDatabase.close();
   });
 
   it("should be able to show user profile", async () => {
-    const userProfile = await request(app).get("/api/v1/profile");
-    expect(1).toBe(1);
+    const userDto = makeUserDto();
+    await request(app).post("/api/v1/users").send(userDto).expect(201);
+    const session = await request(app)
+      .post("/api/v1/sessions")
+      .send({
+        email: userDto.email,
+        password: userDto.password,
+      })
+      .expect(200);
+    const userProfile = await request(app)
+      .get("/api/v1/profile")
+      .set("Authorization", `Bearer ${session.body.token}`);
+
     expect(userProfile.status).toBe(200);
+    expect(userProfile.body).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        name: userDto.name,
+        email: userDto.email,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      })
+    );
+  });
+
+  it("should not be able to show user profile when user does not exists", async () => {
+    const user = makeUser();
+    const { token } = makeJWTToken(user);
+    const expectedError = new ShowUserProfileError();
+
+    const userProfile = await request(app)
+      .get("/api/v1/profile")
+      .set("Authorization", `Bearer ${token}`)
+      .expect(expectedError.statusCode);
+
+    expect(userProfile.body).toEqual(
+      expect.objectContaining({
+        message: expectedError.message,
+      })
+    );
   });
 });
