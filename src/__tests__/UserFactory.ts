8cfa405aadcb6d11ed79963fc4f51f34ad4b1c09
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-02-12 01:00:29 -0300

    :hammer: add makeUser factory function for testing

--- src/__tests__/UserFactory.ts
+++ src/__tests__/UserFactory.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
+import { User } from "../modules/users/entities/User";
 import { ICreateUserDTO } from "../modules/users/useCases/createUser/ICreateUserDTO";
 
 type Override = Partial<ICreateUserDTO>;
@@ -7,6 +9,21 @@ export function makeUserDto(override: Override = {}) {
     email: "any_email@email.com",
     name: "any_name",
     password: "any_password",
+    ...override,
+  };
+}
+
+type OverrideUser = Partial<User>;
+
+export function makeUser(override: OverrideUser = {}) {
+  return {
+    created_at: new Date(),
+    email: "any_email@email.com",
+    name: "any_name",
+    password: "any_password",
+    id: uuidv4(),
+    updated_at: new Date(),
+    statement: [],
     ...override,
   };
 }
