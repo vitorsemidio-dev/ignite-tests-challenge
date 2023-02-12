b20295e80aac8072bd6c15ecd8fe50ce7534a4a1
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-02-12 19:06:02 -0300

    :truck: move makeE2EUser to factory file

--- src/__tests__/UserFactory.ts
+++ src/__tests__/UserFactory.ts
@@ -1,6 +1,8 @@
 import { v4 as uuidv4 } from "uuid";
+import request from "supertest";
 import { User } from "../modules/users/entities/User";
 import { ICreateUserDTO } from "../modules/users/useCases/createUser/ICreateUserDTO";
+import { IAuthenticateUserResponseDTO } from "../modules/users/useCases/authenticateUser/IAuthenticateUserResponseDTO";
 
 type Override = Partial<ICreateUserDTO>;
 
@@ -25,5 +27,30 @@ export function makeUser(override: OverrideUser = {}) {
     updated_at: new Date(),
     statement: [],
     ...override,
+  };
+}
+
+export async function makeE2EUser(
+  appRequest: request.SuperTest<request.Test>,
+  overrideCreateUserDto: Partial<ICreateUserDTO> = {}
+) {
+  const userDto = makeUserDto({
+    ...overrideCreateUserDto,
+  });
+  await appRequest.post("/api/v1/users").send(userDto).expect(201);
+  const { body } = await appRequest.post("/api/v1/sessions").send({
+    email: userDto.email,
+    password: userDto.password,
+  });
+
+  const sessionBody = body as IAuthenticateUserResponseDTO;
+  const user = makeUser({
+    ...sessionBody.user,
+    ...userDto,
+  });
+
+  return {
+    token: sessionBody.token,
+    user,
   };
 }
